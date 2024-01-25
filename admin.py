b3d204ec31702d77b65a70562ddf1c62ccdab352
henry <henrykuzma42@gmail.com>
2024-01-25 18:18:57 -0500

    main in progress

--- admin.py
+++ admin.py
@@ -1,6 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import album
+import numpy as np
+import pandas as pd
 
 def connect():
     cred = credentials.Certificate("../cs3050-warmup.json")
@@ -9,6 +12,20 @@ def connect():
     return db
 
 def main():
+    
+    df = pd.read_json("CS3050-Warmup-Hkuzma-Ljmayo-Jsdrisco-Bmwolf\\rym.json")
+
+    position = df["position"].values
+    release_name = df["release_name"].values
+    artist_name = df["release_name"].values
+    primary_genres = df["primary_genres"].values
+    secondary_genres = df["secondary_genres"].values
+    avg_rating = df["avg_rating"].values
+    rating_count = df["rating_count"].values
+    
+    for i in range(0, len(position)):
+        x = album(position[i], release_name[i], artist_name[i], primary_genres[i], secondary_genres[i], avg_rating[i], rating_count[i])
+
     return 0
 
 if __name__ == "__main__":
