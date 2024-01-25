a0fa9e7facfe624075f995fcb1bc5ae1aa3a484a
Brandon Wolf <brandon.wolf@uvm.edu>
2024-01-25 17:44:08 -0500

    fixed git issues with cred file

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ import json
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("cs3050-warmup.json")
+cred = credentials.Certificate("..\cs3050-warmup.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
