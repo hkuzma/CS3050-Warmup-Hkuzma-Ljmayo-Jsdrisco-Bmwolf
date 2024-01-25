735b5a27d93b96a425206eb6fc3cdc2ad7b4382d
Brandon Wolf <brandon.wolf@uvm.edu>
2024-01-25 17:55:56 -0500

    created a connect function and a main

--- admin.py
+++ admin.py
@@ -2,12 +2,14 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("../cs3050-warmup.json")
-firebase_admin.initialize_app(cred)
+def connect():
+    cred = credentials.Certificate("../cs3050-warmup.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    return db
 
-db = firestore.client()
+def main():
+    return 0
 
-data = {"name": "Los Angeles", "state": "CA", "country": "USA"}
-
-# Add a new doc in collection 'cities' with ID 'LA'
-db.collection("cities").document("LA").set(data)
+if __name__ == "__main__":
+    main()
