abc3a21629e1d49328f8de813fd98cba91c16d38
jsdriscoll8 <jsdriscoll8@gmail.com>
2024-01-25 18:18:26 -0500

    removed redundant constructor

--- album.py
+++ album.py
@@ -8,21 +8,12 @@ class Album:
         self.release_name = release_name
         self.artist_name = artist_name
         self.primary_genres = primary_genres
+
+        # If secondary genres do not exist: do not define (secondary field)
         if pandas.isna(secondary_genres):
             self.secondary_genres = secondary_genres
         self.avg_rating = avg_rating
         self.rating_count = rating_count
-
-        # Initialize a new Album document class with secondary genres
-        def __init__(self, position, release_name, artist_name,
-                     primary_genres, avg_rating,
-                     rating_count):
-            self.position = position
-            self.release_name = release_name
-            self.artist_name = artist_name
-            self.primary_genres = primary_genres
-            self.avg_rating = avg_rating
-            self.rating_count = rating_count
 
     # Return an album description
     def __repr__(self):
