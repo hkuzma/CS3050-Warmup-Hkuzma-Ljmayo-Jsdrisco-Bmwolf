81d07684a721659200eb39efe546d0a2b022490f
henry <henrykuzma42@gmail.com>
2024-01-25 18:29:21 -0500

    pandas robust

--- pandas_read.py
+++ pandas_read.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
 import sys
+try:
+    df = pd.read_json("CS3050-Warmup-Hkuzma-Ljmayo-Jsdrisco-Bmwolf\\rym.json")
+except Exception:
+    df = pd.read_json("rym.json")
 
-df = pd.read_json("CS3050-Warmup-Hkuzma-Ljmayo-Jsdrisco-Bmwolf\\rym.json")
+    
 
 position = df["position"].values
 release_name = df["release_name"].values
@@ -12,3 +16,12 @@ secondary_genres = df["secondary_genres"].values
 avg_rating = df["avg_rating"].values
 rating_count = df["rating_count"].values
 
+print(position)
+print(release_name)
+print(artist_name)
+print(primary_genres)
+print(secondary_genres)
+print(avg_rating)
+print(rating_count)
+
+print(secondary_genres.size)
