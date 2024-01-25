e3721edc33ad104ce626c8e392bff58be423568e
Brandon Wolf <brandon.wolf@uvm.edu>
2024-01-25 17:47:58 -0500

    changes to cred file

--- admin.py
+++ admin.py
@@ -1,11 +1,13 @@
 import firebase_admin
-from firebase_admin import firestore
-import firebase_admin
-import json
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("..\cs3050-warmup.json")
+cred = credentials.Certificate("../cs3050-warmup.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+data = {"name": "Los Angeles", "state": "CA", "country": "USA"}
+
+# Add a new doc in collection 'cities' with ID 'LA'
+db.collection("cities").document("LA").set(data)
