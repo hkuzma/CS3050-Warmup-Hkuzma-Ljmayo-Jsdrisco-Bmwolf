690dd06ddb9e0157264575c5cbcbab39085e4525
henry <henrykuzma42@gmail.com>
2024-01-25 18:05:05 -0500

    panda

--- pandas_read.py
+++ pandas_read.py
@@ -4,5 +4,20 @@ import sys
 
 df = pd.read_json("CS3050-Warmup-Hkuzma-Ljmayo-Jsdrisco-Bmwolf\\rym.json")
 
-print(df.to_string)
+position = df["position"].values
+release_name = df["release_name"].values
+artist_name = df["release_name"].values
+primary_genres = df["primary_genres"].values
+secondary_genres = df["secondary_genres"].values
+avg_rating = df["avg_rating"].values
+rating_count = df["rating_count"].values
 
+#print(position)
+#print(release_name)
+#print(artist_name)
+#print(primary_genres)
+print(secondary_genres)
+#print(avg_rating)
+#print(rating_count)
+
+print(secondary_genres.size)
