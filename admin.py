f47649363903925506effb5c5bea0689b5be5573
Brandon Wolf <brandon.wolf@uvm.edu>
2024-01-25 17:24:06 -0500

    added a db.client

--- admin.py
+++ admin.py
@@ -1,6 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
 cred = credentials.Certificate("cs3050-warmup.json")
 firebase_admin.initialize_app(cred)
 
+db = firestore.client()
