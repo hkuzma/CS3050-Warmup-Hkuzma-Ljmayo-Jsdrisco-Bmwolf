b0d634e0e547918d02c77ab9ddf4a444785be6e9
Brandon Wolf <brandon.wolf@uvm.edu>
2024-02-14 15:18:16 -0500

    comments for admin

--- admin.py
+++ admin.py
@@ -4,7 +4,10 @@ from firebase_admin import firestore
 import numpy as np
 import pandas as pd
 
+# connect function
+# returns a reference to the db
 def connect():
+    # checks to see if a db connection is already initialized, if its not, initialize one
     if not firebase_admin._apps:
         cred = credentials.Certificate("../cs3050-warmup.json")
         firebase_admin.initialize_app(cred)
@@ -14,11 +17,13 @@ def connect():
 def main():
     db = connect()
     
+    #read from the json
     try:
         df = pd.read_json("CS3050-Warmup-Hkuzma-Ljmayo-Jsdrisco-Bmwolf\\rym.json")
     except Exception:
         df = pd.read_json("rym.json")
 
+    # parse everything
     position = df["position"].values
     release_name = df["release_name"].values
     artist_name = df["artist_name"].values
@@ -27,20 +32,27 @@ def main():
     avg_rating = df["avg_rating"].values
     rating_count = df["rating_count"].values
     
+    # go through the data
     for i in range(0, len(position)):
+        # if there isnt a secondary genre, dont add that to the data base
         if pd.isna(secondary_genres[i]):
+            # turn primary genres into a list
             primary_genres[i] = str(primary_genres[i])
             genreList = primary_genres[i].split(", ")
+            # data to be sent
             data = {"position": int(position[i]), "album_name": str(release_name[i]), "artist_name": str(artist_name[i]), 
                     "primary_genres": genreList, "avg_rating": float(avg_rating[i]), "rating_count": int(rating_count[i])}
         else:
+            # turn primary and secondary genres into lists
             primary_genres[i] = str(primary_genres[i])
             primaryGenreList = primary_genres[i].split(", ")
             secondary_genres[i] = str(secondary_genres[i])
             secondaryGenresList = secondary_genres[i].split(", ")
             data = {"position": int(position[i]), "album_name": str(release_name[i]), "artist_name": str(artist_name[i]), 
                     "primary_genres": primaryGenreList, "secondary_genres": secondaryGenresList, "avg_rating": float(avg_rating[i]), "rating_count": int(rating_count[i])}
+        # send the data
         db.collection("rym").document(str(position[i])).set(data)
+    # close the database
     db.close()
     return 0
 
