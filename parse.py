cfe98e2b9777bf755ae7b57b119d1e4d8e38cccf
henry <henrykuzma42@gmail.com>
2024-02-14 14:56:37 -0500

    comments added to parse

--- parse.py
+++ parse.py
@@ -21,7 +21,8 @@ def main():
     while not quit:
         user_input = input("")
         user_input = user_input.strip()
-
+        
+        #handle commands from user input
         match user_input:
             case "--quit":
                 quit = True
@@ -31,21 +32,30 @@ def main():
                 print_example()
             case _:
                 try:
+                    #split string by spaces
+                    #maintain quoted strings as 1 list item
                     input_storage = shlex.split(user_input, posix=False)
+                    
+                    #validate that query exists
                     if len(input_storage) == 0:
                         print("Invalid - empty query")
-                    elif input_storage[0] != "??":
+                    elif input_storage[0] != "??": 
                         print("Missing ?? at start - please enter a valid query")
                     else:
+                        #handle query in depth
                         result = handle_query(input_storage[1:])
+                        
+                        #if query is valid --> send to query.py
                         if type(result) is list:
                             q = query(result)
                             print_query(q)
+                        #if query invalid --> print error message
                         else:
                             print(result)
+                #if query would break, print error message
                 except ValueError:
                     print("Invalid query - ensure your quotation is correct")
-
+    #end runtime
     print("Exiting")
 
 # Print language description and commands for the user.
@@ -101,38 +111,45 @@ def handle_query(query: list):
         second_half = query.index("&&") + 1
         compound_query = handle_query(query[second_half:])
         first_half = query[:second_half]
-
+        
+        #check that first half of query fits language format
         if len(first_half) != 4:
             return "Invalid query - check quotation and number of inputs"
 
+        #TO DO: add comment
         if type(compound_query) is list:
             first_half.extend(compound_query)
             return first_half
+        
+        #TO DO: add comment
         else:
             return compound_query
+    
+    #Handle OR operator    
     elif "||" in query:
         second_half = query.index("||") + 1
         compound_query = handle_query(query[second_half:])
         first_half = query[:second_half]
+        
+        #TO DO: add comment
         if type(compound_query) is list:
             first_half.extend(compound_query)
             return first_half
+        
+        #TO DO: add comment
         else:
             return compound_query
     else:
+        #check that query fits language format
         if len(query) > 3:
             return "Invalid query - too many inputs"
         else:
             return query
    
 
-
-    
-    '''else:    
-        for i in range(input_storage):
-            if input_storage[i] == "==":        
-                print(input_storage) '''
+#Print dictionary in a readable format
 def print_query(query):
+    #clead up data returned from Query.py
     for i in query:
         if i == "No Data":
             if len(query) == 1:
@@ -140,6 +157,7 @@ def print_query(query):
                 return
             else:
                 pass
+        #Print data
         else:
             print(f"\n{i.get('position')}). Album Name: {i.get('album_name')} By {i.get('artist_name')} with avg rating: {i.get('avg_rating')}")
             print(f"Primary Genres: {i.get('primary_genres')}")
